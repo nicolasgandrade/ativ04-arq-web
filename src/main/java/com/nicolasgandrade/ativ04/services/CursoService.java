bd6b070585c8fdf1b3c4cf0f6218af2b9ee96971
Felipe Sanches Franqueira <felipe04sfr@outlook.com>
2024-05-15 22:06:52 -0300

    requisito1

--- src/main/java/com/nicolasgandrade/ativ04/services/CursoService.java
+++ src/main/java/com/nicolasgandrade/ativ04/services/CursoService.java
@@ -1,10 +1,13 @@
 package com.nicolasgandrade.ativ04.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicolasgandrade.ativ04.dtos.CursoRequestDTO;
 import com.nicolasgandrade.ativ04.models.Curso;
+import com.nicolasgandrade.ativ04.models.Professor;
 import com.nicolasgandrade.ativ04.repositories.CursoRepository;
 
 @Service
@@ -22,4 +25,7 @@ public class CursoService {
         return cursoRepository.save(curso);
     }
 
+    public List<Professor> findProfessoresByCursoId(int cursoId) {
+        return cursoRepository.findProfessoresByCursoId(cursoId);
+    }
 }

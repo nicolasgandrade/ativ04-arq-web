23e1c91b81339cc9aa3f5477d25ea53a8106c41e
Felipe Sanches Franqueira <felipe04sfr@outlook.com>
2024-05-21 20:15:02 -0300

    requisito3

--- src/main/java/com/nicolasgandrade/ativ04/repositories/TreinamentoRepository.java
+++ src/main/java/com/nicolasgandrade/ativ04/repositories/TreinamentoRepository.java
@@ -1,9 +1,24 @@
 package com.nicolasgandrade.ativ04.repositories;
 
+import com.nicolasgandrade.ativ04.models.Professor;
 import com.nicolasgandrade.ativ04.models.Treinamento;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TreinamentoRepository extends JpaRepository<Treinamento, Integer> {
+
+    @Query("SELECT p FROM Professor p WHERE p.id NOT IN " +
+            "(SELECT t.professor.id FROM Treinamento t " +
+            "WHERE (t.inicio BETWEEN :dataInicio AND :dataFim) " +
+            "OR (t.fim BETWEEN :dataInicio AND :dataFim))")
+    List<Professor> findProfessoresLivres(
+            @Param("dataInicio") LocalDateTime dataInicio,
+            @Param("dataFim") LocalDateTime dataFim);
 }
